dde4c3617da96e29f32e01a07639b604e9b659bc
roferrei <tecnicorodrigo@outlook.com>
2022-08-28 15:47:57 +0200

    update some files

---
+++ ft_printf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 14:51:07 by roferrei          #+#    #+#             */
+/*   Updated: 2022/08/28 14:55:31 by roferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h> //The include file <stdarg.h> declares a type va_list and defines three macros for stepping through a list of arguments whose number and types are not known to the called function
+# include <>
+
+#endif
---
+++ src/is_hex.c
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_hex.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 15:35:31 by roferrei          #+#    #+#             */
+/*   Updated: 2022/08/28 15:38:46 by roferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// %p The void * pointer argument has to be printed in hexadecimal format.
+// %x Prints a number in hexadecimal (base 16) lowercase format.
+// %X Prints a number in hexadecimal (base 16) uppercase format.
