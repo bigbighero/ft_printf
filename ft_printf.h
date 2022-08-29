c6ffebd0d7ad53a0a52caf6f59f009ea2c87edc2
roferrei <tecnicorodrigo@outlook.com>
2022-08-30 00:03:07 +0200

    update some files

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,15 @@
 /*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 14:51:07 by roferrei          #+#    #+#             */
-/*   Updated: 2022/08/29 19:58:51 by roferrei         ###   ########.fr       */
+/*   Updated: 2022/08/30 00:02:58 by roferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stdarg.h> //The include file <stdarg.h> declares a type va_list and defines three macros for stepping through a list of arguments whose number and types are not known to the called function
-# include <>
+# include <stdlib.h> 
+# include <unistd.h> // This include file is used here to use write function.
+# include <stdarg.h> //This include file declares a type va_list and defines three macros for stepping through a list of arguments whose number and types are not known to the called function
 
 #endif
